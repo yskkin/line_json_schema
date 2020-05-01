38f632fe157b3ebd59549052438c18708f9ae0db
Yoshiyuki Kinjo <yskkin@gmail.com>
2020-05-01 19:47:43 +0900

    implement quickReply

--- __tests__/messaging/message.js
+++ __tests__/messaging/message.js
@@ -1,7 +1,9 @@
-const jestJsonSchema = require('jest-json-schema');
-expect.extend(jestJsonSchema.matchers);
+const { matchersWithOptions }= require('jest-json-schema');
 
 const message = require('messaging/message.schema.json');
+const action = require('messaging/action.schema.json');
+
+expect.extend(matchersWithOptions({ schemas: [action]}));
 
 test('with sender', () => {
     const sticker = { type: 'sticker', packageId: '1', stickerId: '1' };
@@ -13,6 +15,24 @@ test('with sender', () => {
     expect({ ...sticker, sender: { name: 'foo', iconUrl: 'invalid_url' }}).not.toMatchSchema(message);
     expect({ ...sticker, sender: { name: 'foo', iconUrl: `https://${Array(1000).fill('a').join('')}` }}).not.toMatchSchema(message);
     expect({ ...sticker, sender2: { name: 'foo', iconUrl: 'https://example.org/icon.jpg' }}).not.toMatchSchema(message);
+});
+
+test('with quickReply', () => {
+    const sticker = { type: 'sticker', packageId: '1', stickerId: '1' };
+    expect({ ...sticker, quickReply: {
+        items: [
+            { type: 'action', action: { type: 'postback', label: 'test label', data: 'test data', displayText: 'test message' } },
+            { type: 'action', action: { type: 'message', label: 'test label', text: 'test message' } },
+            { type: 'action', action: { type: 'uri', label: 'test label', uri: 'https://example.com' } },
+            { type: 'action', action: { type: 'datetimepicker', label: 'test label', data: 'test data', mode: 'date' }},
+            { type: 'action', action: { type: 'datetimepicker', label: 'test label', data: 'test data', mode: 'time' }},
+            { type: 'action', action: { type: 'datetimepicker', label: 'test label', data: 'test data', mode: 'datetime', initial: '2020-01-01T12:34:56' }},
+            { type: 'action', action: { type: 'location', label: 'test label' } },
+            { type: 'action', action: { type: 'camera', label: 'test label' } },
+            { type: 'action', action: { type: 'cameraRoll', label: 'test label' } }
+        ]
+
+    }}).toMatchSchema(message);
 });
 
 test('sticker message', () => {
